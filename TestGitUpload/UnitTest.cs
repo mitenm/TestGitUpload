7efb726ea52205aef556bdc56c445c14df55ebb0
mitenm <miten.morakhia@gmail.com>
2016-08-04 21:02:27 +0530

    Initial commit
    Initial commit

---
+++ TestGitUpload/UnitTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace TestGitUpload
+{
+    [TestFixture]
+    class UnitTest
+    {
+        [Test]
+        public void SampleUnitTest()
+        {
+            Assert.AreEqual(1,1);
+            Assert.AreNotEqual(1,2);
+            Assert.IsFalse(1==2);
+            Assert.IsTrue(true);
+        }
+    }
+}
